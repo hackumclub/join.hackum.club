1fe0f9207e1d2fb30724e3fdf2e05ec7c5f8fd05
Ganbold <g.h.ganbold13@gmail.com>
2022-03-22 20:31:26 +0800

    added: Navigation component, Logo.svg, package-lock.json

---
+++ components/Navigation/index.js
@@ -0,0 +1,34 @@
+import styled from "@emotion/styled";
+import { css, jsx } from "@emotion/react";
+
+const Nav = styled.header`
+  display: flex;
+  aling-items: center;
+  justify-content: space-between;
+  margin: 0 24px;
+  padding: 5px 24px;
+  height: 72px;
+
+  @media (min-width: 640px) {
+    margin: auto;
+    margin: 0 36px;
+    height: 85px;
+  }
+`;
+const Logo = styled.img`
+  width: 108px;
+  @media (min-width: 640px) {
+    width: 136px;
+  }
+`;
+
+const Header = () => {
+  return (
+    <Nav>
+      <Logo src="/Logo.svg"></Logo>
+      <button>Холбогдох</button>
+    </Nav>
+  );
+};
+
+export default Header;
